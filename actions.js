47e8b63d6dc65697cd1322872ddda480b8ea21a0
Ivan Calvet <ivancalvet@gmail.com>
2014-12-07 12:07:34 +0100

    Cleaning of the main javascript file

--- actions.js
+++ actions.js
@@ -2,6 +2,38 @@ var login = localStorage['Login'];
 var password = localStorage['Password'];
 var URL = localStorage['URL'];
 
+function printMessage(message) {
+    $("#Message").html(chrome.i18n.getMessage(message));
+    $("#Loading").fadeOut('slow');
+    $("#Message").fadeIn('fast').delay(1000).fadeOut('slow');
+};
+
+function create1Note(id, content) {
+    var note = document.createElement('div');
+    note.className = 'Note';
+    note.style.display = 'none';
+    note.contentEditable = true;
+    if (id) note.id = id;
+    content = content.split('\n');
+    for (var c=0; c < content.length; c++) $(note).append(content[c]+'<br>');
+    $(note).blur(function() {
+        $("#Loading").fadeIn("fast");
+        var cont = this.innerHTML.split('<br>');
+        for (var i=cont.length-1; i>0; i--) {
+            if (cont[i] == '') cont.splice(i,1);
+            else break;
+        }
+        cont = cont.join('\n')
+        if (cont == "") {
+            $(this).slideUp();
+            Notes.delete1Note(this);
+        } else Notes.save1Note(this,cont);
+    });
+    $('body').append(note);
+    $(note).slideDown();
+    return note;
+};
+
 var Notes = {
     requestNotes: function() {
         $.ajax({
@@ -12,40 +44,12 @@ var Notes = {
  	        widthCredentials: true
  	    },
  	    success: function (notes) {
-                $("#Loading").hide();
-                $("#Message").html(chrome.i18n.getMessage("welcome"));
-                $("#Message").fadeIn("fast").delay(1000).fadeOut('slow');
-                for (var n in notes) {
-                    var note = document.createElement('div');
-                    note.className = 'Note';
-                    note.id = notes[n]['id'];
-                    var content = notes[n]['content'];
-                    content = content.split('\n');
-                    for (var c=0; c < content.length; c++) {
-                        note.innerHTML += content[c]+'<br>'
-                    }
-                    note.contentEditable = true;
-					note.addEventListener('blur', function () {
-                        $("#Loading").fadeIn("fast");
-                        var cont = this.innerHTML.split('<br>');
-                        for (var i=cont.length-1; i>0; i--) {
-                            if (cont[i] == '') cont.splice(i,1);
-                            else break;
-                        }
-                        cont = cont.join('\n')
-                        if (cont == "") {
-							$("#"+this.id).slideUp();
-							Notes.delete1Note(this.id);
-						} else Notes.save1Note(this.id,cont);
-					});
-                    document.body.appendChild(note);
-                }
+            for (var n in notes) create1Note(notes[n]['id'],notes[n]['content']);
+            printMessage("welcome");
  	    },
  	    error: function () {
-            $("#Message").html(chrome.i18n.getMessage("error_connection"));
             $("#NewNote").hide();
-            $("#Loading").hide();
-            $("#Message").fadeIn("fast");
+            printMessage("error_connection");
  	    },
 		timeout: 3000,
  	    beforeSend: function (xhr) {
@@ -54,118 +58,93 @@ var Notes = {
  	    }
         });
     },
-	save1Note: function(id, content) {
-	  $.ajax({
- 	    type: 'PUT',
- 	    url: localStorage['URL'] + "/index.php/apps/notes/api/v0.2/notes/"+id+'?content='+encodeURIComponent(content),
- 	    contentType: 'application/json',
- 	    xhrFields: {
- 	        widthCredentials: true
- 	    },
- 	    success: function () {
-            $("#Loading").hide();
-            $("#Message").html(chrome.i18n.getMessage("note_saved"));
-            $("#Message").fadeIn("fast").delay(1000).fadeOut('slow');
- 	    },
- 	    error: function () {
-            $("#Loading").hide();
-            $("#Message").html(chrome.i18n.getMessage("error"));
-            $("#Message").fadeIn("fast").delay(1000).fadeOut('slow');
- 	    },
- 	    beforeSend: function (xhr) {
- 	        var auth = btoa(login + ':' + password);
- 	        xhr.setRequestHeader('Authorization', 'Basic ' + auth);
- 	    }
-      });
+	save1Note: function(note, content) {
+	    $.ajax({
+            type: 'PUT',
+            url: localStorage['URL'] + "/index.php/apps/notes/api/v0.2/notes/"+note.id+'?content='+encodeURIComponent(content),
+            contentType: 'application/json',
+            xhrFields: {
+                widthCredentials: true
+            },
+            success: function () {
+                printMessage("note_saved");
+            },
+            error: function () {
+                printMessage("error");
+            },
+            beforeSend: function (xhr) {
+                var auth = btoa(login + ':' + password);
+                xhr.setRequestHeader('Authorization', 'Basic ' + auth);
+            }
+        });
 	},
-    delete1Note: function(id) {
-      $.ajax({
-        type: 'DELETE',
-        url: localStorage['URL'] + "/index.php/apps/notes/api/v0.2/notes/"+id,
-        contentType: 'application/json',
-        xhrFields: {
-            widthCredentials: true
-        },
-        success: function () {
-            $("#Loading").hide();
-            $("#Message").html(chrome.i18n.getMessage("note_deleted"));
-            $("#Message").fadeIn("fast").delay(1000).fadeOut('slow');
-        },
-        error: function () {
-			$("#"+this.id).slideDown();
-            $("#Loading").hide();
-            $("#Message").html(chrome.i18n.getMessage("error"));
-            $("#Message").fadeIn("fast").delay(1000).fadeOut('slow');
-        },
-        beforeSend: function (xhr) {
-            var auth = btoa(login + ':' + password);
-            xhr.setRequestHeader('Authorization', 'Basic ' + auth);
-        }
-      });
-    },
     add1Note: function(note) {
-      $.ajax({
-        type: 'POST',
-        url: localStorage['URL'] + "/index.php/apps/notes/api/v0.2/notes",
-        contentType: 'application/json',
-        xhrFields: {
-            widthCredentials: true
-        },
-        success: function (newnote) {
-            note.id = newnote['id'];
-            $("#Message").html(chrome.i18n.getMessage("new_note_added"));
-            $("#Loading").hide();
-            $("#Message").fadeIn("fast").delay(1000).fadeOut('slow');
-        },
-        error: function () {
-			note.slideUp();
-            $("#Loading").hide();
-            $("#Message").html(chrome.i18n.getMessage("error"));
-            $("#Message").fadeIn("fast").delay(1000).fadeOut('slow');
-        },
-        beforeSend: function (xhr) {
-            var auth = btoa(login + ':' + password);
-            xhr.setRequestHeader('Authorization', 'Basic ' + auth);
-        }
-      });
+        $.ajax({
+            type: 'POST',
+            url: localStorage['URL'] + "/index.php/apps/notes/api/v0.2/notes",
+            contentType: 'application/json',
+            xhrFields: {
+                widthCredentials: true
+            },
+            success: function (newnote) {
+                note.id = newnote['id'];
+                printMessage("new_note_added");
+            },
+            error: function () {
+                note.slideUp();
+                printMessage("error");
+            },
+            beforeSend: function (xhr) {
+                var auth = btoa(login + ':' + password);
+                xhr.setRequestHeader('Authorization', 'Basic ' + auth);
+            }
+        });
+    },
+    delete1Note: function(note) {
+        $.ajax({
+        type: 'DELETE',
+            url: localStorage['URL'] + "/index.php/apps/notes/api/v0.2/notes/"+note.id,
+            contentType: 'application/json',
+            xhrFields: {
+                widthCredentials: true
+            },
+            success: function () {
+                printMessage("note_deleted");
+            },
+            error: function () {
+                $(note).slideDown();
+                printMessage("error");
+            },
+            beforeSend: function (xhr) {
+                var auth = btoa(login + ':' + password);
+                xhr.setRequestHeader('Authorization', 'Basic ' + auth);
+            }
+        });
     },
 };
 
 document.addEventListener('DOMContentLoaded', function () {
+    // Check parameters
     if (!URL || !login || !password) {
         window.location.replace("options.html");
     }
-    $("#Loading").fadeIn("fast");
+
+    // Request the current notes
     Notes.requestNotes();
-    $("#NewNote").click(function() {
-        $("#Loading").fadeIn("fast");
-		var note = document.createElement('div');
-		note.className = 'Note';
-		note.contentEditable = true;
-		note.style.display = 'none';
-		note.addEventListener('blur', function () {
-			$("#Loading").fadeIn("fast");
-			var cont = this.innerHTML.split('<br>');
-			for (var i=cont.length-1; i>0; i--) {
-				if (cont[i] == '') cont.splice(i,1);
-				else break;
-			}
-			cont = cont.join('\n')
-			if (cont == "") {
-				$("#"+this.id).slideUp();
-				Notes.delete1Note(this.id);
-			} else Notes.save1Note(this.id,cont);
-		});
-		document.body.appendChild(note);
-		$(note).slideDown();
-		note.focus();
-        Notes.add1Note(note);
-    });
+
+    // Header
+    $('#header').fadeIn('slow');
+    $("#Loading").fadeIn("fast");
     $("#NewNote").hover(function() {
             $("#Message").html(chrome.i18n.getMessage("add_new_note"));
             $("#Message").fadeIn("fast");
         },
         function() {
             $("#Message").fadeOut("fast");
+    });
+    $("#NewNote").click(function() {
+        var note = create1Note(null,'');
+        note.focus();
+        Notes.add1Note(note);
     });
 });
